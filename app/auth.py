f24d1b36ab1e316547557fc439bd6d735e38916e
m0r-h2 <h2q3internet@gmail.com>
2025-10-04 17:04:46 +0300

    создание refresh эндпоинта в routers/user

--- app/auth.py
+++ app/auth.py
@@ -15,6 +15,8 @@ from app.db_depends import get_async_db
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+REFRESH_TOKEN_EXPIRE_DAYS = 7
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/token")
 
 def hash_password(password: str) -> str:
@@ -44,6 +46,15 @@ def create_access_token(data: dict):
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def create_refresh_token(data: dict):
+    """
+    Создаёт рефреш-токен с длительным сроком действия.
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
 
 
 async def get_current_user(token: str = Depends(oauth2_scheme),
