60da7951514376e78b8228118556629f8626120f
m0r-h2 <h2q3internet@gmail.com>
2025-10-10 19:15:45 +0300

    настройка докер файла

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:2532@localhost:5432/ecommerce_db"
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:2532@db:5432/ecommerce_db"
 
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
