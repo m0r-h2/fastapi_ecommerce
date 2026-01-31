9cab20b88d87eed022a307415a6e386e80c4a15e
m0r-h2 <h2q3internet@gmail.com>
2026-01-31 21:22:06 +0300

    add migrations

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 from alembic import context
 
 from app.database import Base
-from app import models
+
+import app.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
